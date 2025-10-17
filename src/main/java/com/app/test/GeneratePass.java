3836fb90a2c17d5cb0a77e34c5f4841ad264b51e
nd.tuan <nd.tuan>
2025-10-17 15:41:45 +0700

    u

--- src/main/java/com/app/test/GeneratePass.java
+++ src/main/java/com/app/test/GeneratePass.java
@@ -1,12 +1,13 @@
 package com.app.test;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class GeneratePass {
 	
 	public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "tuannd";
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        String rawPassword = "123456";
         String encodedPassword = encoder.encode(rawPassword);
         System.out.println("BCrypt password: " + encodedPassword);
     }
